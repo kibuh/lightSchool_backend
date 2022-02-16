04bcece1e5c050af7d236a2f576245e996f36843
kibuh <gkibuhguy@gmail.com>
2022-02-16 17:54:22 +0100

    initial project

---
+++ src/test/java/com/lightschool/org/lightSchool_backend/LightSchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightschool.org.lightSchool_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightSchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
